63a15d44af2f6796a370c786f157cba113bc6a32
shiyuetc <shiyutako@gmail.com>
2026-05-07 10:38:54 +0900

    feat: add header navigation links in welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
-@section('content')
-<div>
-    <h1>Welcome to Utakeep</h1>
+@section('header-nav')
+<div class="flex gap-2">
+    <a href="/login" class="px-4 py-1.5 text-sm text-gray-700 hover:bg-gray-50 transition">ログイン</a>
+    <a href="/register" class="px-4 py-1.5 text-sm bg-primary text-primary-light hover:bg-primary-hover transition">新規登録</a>
 </div>
+@endsection
+@section('content')
 @endsection
