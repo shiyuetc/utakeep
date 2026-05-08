5b54efd02236ed43edd35fb846428118ecfd4142
shiyuetc <shiyutako@gmail.com>
2026-05-08 14:42:26 +0900

    fix: keep sidebar fixed while scrolling main content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
         @include('layouts.partials.head')
         @livewireStyles
     </head>
-    <body class="bg-gray-50 text-gray-900 antialiased min-h-screen flex flex-col">
+    <body class="bg-gray-50 text-gray-900 antialiased min-h-screen md:h-screen md:overflow-hidden flex flex-col">
         @include('layouts.partials.header')
-        <main class="flex-1">
-            <div class="max-w-5xl mx-auto p-2 pb-24 flex gap-2">
-                <aside class="hidden md:block w-64 flex-shrink-0">
+        <main class="flex-1 md:min-h-0">
+            <div class="max-w-5xl mx-auto p-2 pb-24 md:px-2 md:py-0 flex gap-2 md:h-full md:min-h-0 md:overflow-hidden">
+                <aside class="hidden md:block w-64 flex-shrink-0 md:py-2">
                     @include('layouts.partials.sidebar')
                 </aside>
-                <section class="w-full flex-1 min-w-0">
+                <section class="w-full flex-1 min-w-0 md:h-full md:overflow-y-auto md:py-2 md:pr-1">
                     @yield('content')
                 </section>
             </div>
