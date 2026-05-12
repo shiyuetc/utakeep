3d1b4afe6ed039568e23d6c1ca42dca54e8c602d
shiyuetc <shiyutako@gmail.com>
2026-05-12 21:16:50 +0900

    feat: 言語ファイルを日本語化

--- lang/ja/auth.php
+++ lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ユーザーIDまたはパスワードが正しくありません。',
+    'password' => 'パスワードが正しくありません。',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds 秒後に再度お試しください。',
 
 ];
