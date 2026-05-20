123bad27436f5325bc808762fca5bfadf542315f
shiyuetc <shiyutako@gmail.com>
2026-05-20 23:11:07 +0900

    refactor: Wrap route definitions in middleware groups for better organization

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,29 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    if (auth()->check()) {
-        return view('pages.home');
-    }
-    return view('pages.welcome');
-})->name('home')->middleware('web');
+Route::middleware(['web'])->group(function () {
+    Route::get('/', function () {
+        if (auth()->check()) {
+            return view('pages.home');
+        }
+        return view('pages.welcome');
+    })->name('home');
+});
 
-Route::get('/songs', function () {
-    return view('pages.song.index');
-})->name('songs')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/songs', function () {
+        return view('pages.song.index');
+    })->name('songs');
 
-Route::get('/users', function () {
-    return view('pages.user.index');
-})->name('users.index')->middleware('auth');
+    Route::get('/users', function () {
+        return view('pages.user.index');
+    })->name('users.index');
 
-Route::get('/@{user:screen_name}', function (User $user) {
-    return view('pages.user.show', ['user' => $user]);
-})->name('users.show')->middleware('auth');
+    Route::get('/@{user:screen_name}', function (User $user) {
+        return view('pages.user.show', ['user' => $user]);
+    })->name('users.show');
 
-Route::get('/settings', function () {
-    return view('pages.settings');
-})->name('settings')->middleware('auth');
+    Route::get('/settings', function () {
+        return view('pages.settings');
+    })->name('settings');
+});
