65205723bb96dbdb058841361f32094f6b671aaa
shiyuetc <shiyutako@gmail.com>
2026-05-21 22:50:49 +0900

    feat: faviconを追加

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -1,6 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>@hasSection('page-title')@yield('page-title') - {{ config('app.name') }}@else{{ config('app.name') }}@endif</title>
+<link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link rel="stylesheet" href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css">
