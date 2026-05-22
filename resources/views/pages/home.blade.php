18749b58e49191b5479be7bcee6a277570a7c8ca
shiyuetc <shiyutako@gmail.com>
2026-05-22 19:39:01 +0900

    feat: メールアドレス非認証時にはホームにアラートを表示するように

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 @section('page-title', 'ホーム')
 @section('content')
+@if (! auth()->user()->hasVerifiedEmail())
+    <x-ui.alert type="default">メールアドレスが未認証です。登録時に送信されたメールから認証を完了してください。</x-ui.alert>
+@endif
 <livewire:features.activity.timeline />
 @endsection
